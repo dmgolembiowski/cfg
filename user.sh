1325911b34da9e18a827e3c692cee71d6355a79f
Eivind Uggedal <eivind@uggedal.com>
2019-03-18 13:29:45 +0100

    user: dotfiles

---
+++ user.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+ROOT=$(cd "$(dirname "$0")"; pwd -P)
+DOTFILES=$ROOT/dotfiles
+
+##
+## Dotfiles
+##
+
+find $DOTFILES -type f -print0 | while IFS= read -r -d '' f; do
+	rel=${f##$DOTFILES/}
+	src="$DOTFILES/$rel"
+	dst="$HOME/$rel"
+
+	mkdir -p "$(dirname "$dst")"
+
+	if ! [ -L "$dst" ]; then
+		ln -sf "$src" "$dst"
+	fi
+done
+
