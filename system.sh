e9bf7e93bfd0d33ac6166abf758a293b63eed72f
Eivind Uggedal <eivind@uggedal.com>
2019-03-18 14:21:40 +0100

    system: f function

--- system.sh
+++ system.sh
@@ -1,9 +1,30 @@
 #!/bin/sh -e
 
+ROOT=$(cd "$(dirname "$0")"; pwd -P)
+FILES=$ROOT/files
+
+diff() {
+	if command -v git >/dev/null; then
+		git diff --no-index "$@"
+	else
+		comamnd diff -u "$@"
+	fi
+}
+
 p() {
 	for p; do
 		pacman -Q $p >/dev/null 2>&1 || pacman -S $p
 	done
+}
+
+f() {
+	local dst=$1
+	local src=$FILES$1
+	mkdir -p $(dirname $dst)
+	if [ -e $dst ]; then
+		diff $dst $src
+	fi
+	cp $src $dst
 }
 
 ##
