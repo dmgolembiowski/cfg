b9eca4b0f161efcc6295adb52517c3f73f009ce5
Eivind Uggedal <eivind@uggedal.com>
2019-09-16 10:24:58 +0000

    updates: function

--- updates.sh
+++ updates.sh
@@ -4,17 +4,24 @@ ROOT=$(cd "$(dirname "$0")"; pwd -P)
 
 . $ROOT/ver.sh
 
-for n in prometheus alertmanager node_exporter; do
-	_latest=$(
-		curl -s https://api.github.com/repos/prometheus/$n/releases/latest |
+
+gh() {
+	local u="$1"
+	local n="$2"
+	local _latest=$(
+		curl -s https://api.github.com/repos/$u/$n/releases/latest |
 			jq .tag_name -j |
 			sed 's/^v//'
 	)
 
-	eval _cur=\$$(echo $n | tr '[a-z]' '[A-Z]')_V
+	eval local _cur=\$$(echo $n | tr '[a-z]' '[A-Z]')_V
 	if [ "$_cur" != "$_latest" ]; then
 		echo $n $_cur '->' $_latest
 	fi
+}
+
+for n in prometheus alertmanager node_exporter; do
+	gh prometheus $n
 done
 
 _kubectl_latest=$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)
