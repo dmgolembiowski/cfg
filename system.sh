01e4f95f6f263941a0083ab847f3eece24d05633
Eivind Uggedal <eivind@uggedal.com>
2019-09-24 15:13:39 +0200

    fwupd: allow efi updates

--- system.sh
+++ system.sh
@@ -69,6 +69,9 @@ elif distro arch; then
 	# installed packages:
 	file /etc/systemd/system/paccache.service.d/override.conf
 	svc paccache.timer
+
+	file /etc/fwupd/uefi.conf
+	file /etc/pacman.d/hooks/fwupd.hook
 fi
 
 pkg '
