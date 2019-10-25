0d4f6f8a855a9cfbe85ca13476e91e2f094b1b59
Eivind Uggedal <eivind@uggedal.com>
2019-10-25 14:21:31 +0200

    mac: need PyYAML

--- mac.sh
+++ mac.sh
@@ -2,6 +2,13 @@
 
 ROOT=$(cd "$(dirname "$0")"; pwd -P)
 
+for _p in PyYAML==5.1.2 ; do
+	if ! pip3 freeze | grep -q "^$_p"; then
+		tmpcache=$(mktemp -d)
+		pip3 --cache-dir=$tmpcache install --user $_p
+	fi
+done
+
 . $ROOT/lib.sh
 
 brew bundle
