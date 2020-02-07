b936efd6f2a847bf6ff158f27f27e79d267d1028
Eivind Uggedal <eivind@uggedal.com>
2020-02-07 14:24:37 +0100

    polybar: start with X and remove dupe emacs modeline

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -85,12 +85,7 @@
   (exwm-randr-enable)
 
   (require 'exwm-config)
-  (exwm-config-default)
-
-  (display-battery-mode 1)
-  (setq display-time-default-load-average nil
-	display-time-format "%Y-%m-%d %H:%M")
-  (display-time-mode t))
+  (exwm-config-default))
 
 (use-package desktop-environment
   :ensure t
