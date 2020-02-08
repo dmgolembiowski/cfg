a10f689845dc10845388f7958fdadc3b643be32c
Eivind Uggedal <eivind@uggedal.com>
2020-02-09 00:03:42 +0100

    emacs: indent with spaces

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -19,8 +19,10 @@
 ;;
 
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (package-initialize)
 
 (unless package-archive-contents
@@ -33,26 +35,35 @@
 (require 'use-package)
 
 
+;;
+;; Built-in packages
+;;
+
 (use-package org
-	     :bind (("C-C a" . org-agenda))
-	     :init (setq
-		    org-agenda-files '("~/src/org/")
-		    org-todo-keywords '((sequence
-					 "TODO"
-					 "IN-PROGRESS"
-					 "WAITING"
-					 "|"
-					 "DONE"
-					 "CANCELED"))
-		    org-tag-alist '((:startgroup)
-				    ("@home" . ?h)
-				    ("@office" . ?o)
-				    (:endgroup)
-				    ("errand" . ?e)
-				    ("computer" . ?c)
-				    ("phone" . ?p))
-		    org-startup-folded 'content
-		    org-startup-indented t))
+  :bind (("C-C a" . org-agenda))
+  :init (setq
+         org-agenda-files '("~/src/org/")
+         org-todo-keywords '((sequence
+                              "TODO"
+                              "IN-PROGRESS"
+                              "WAITING"
+                              "|"
+                              "DONE"
+                              "CANCELED"))
+         org-tag-alist '((:startgroup)
+                         ("@home" . ?h)
+                         ("@office" . ?o)
+                         (:endgroup)
+                         ("errand" . ?e)
+                         ("computer" . ?c)
+                         ("phone" . ?p))
+         org-startup-folded 'content
+         org-startup-indented t))
+
+
+;;
+;; Third-party packages
+;;
 
 (use-package magit
   :ensure t
@@ -66,20 +77,20 @@
 
   (defun exwm-change-screen-hook ()
     (let ((xrandr-output-regexp "\n\\([^ ]+\\) connected ")
-	  default-output)
+          default-output)
       (with-temp-buffer
-	(call-process "xrandr" nil t nil)
-	(goto-char (point-min))
-	(re-search-forward xrandr-output-regexp nil 'noerror)
-	(setq default-output (match-string 1))
-	(forward-line)
-	(if (not (re-search-forward xrandr-output-regexp nil 'noerror))
-	    (call-process "xrandr" nil nil nil "--output" default-output "--auto")
-	  (call-process
-	   "xrandr" nil nil nil
-	   "--output" (match-string 1) "--primary" "--auto"
-	   "--output" default-output "--off")
-	  (setq exwm-randr-workspace-output-plist (list 0 (match-string 1)))))))
+        (call-process "xrandr" nil t nil)
+        (goto-char (point-min))
+        (re-search-forward xrandr-output-regexp nil 'noerror)
+        (setq default-output (match-string 1))
+        (forward-line)
+        (if (not (re-search-forward xrandr-output-regexp nil 'noerror))
+            (call-process "xrandr" nil nil nil
+                          "--output" default-output "--auto")
+          (call-process "xrandr" nil nil nil
+                        "--output" (match-string 1) "--primary" "--auto"
+                        "--output" default-output "--off")
+          (setq exwm-randr-workspace-output-plist (list 0 (match-string 1)))))))
 
   (add-hook 'exwm-randr-screen-change-hook 'exwm-change-screen-hook)
   (exwm-randr-enable)
@@ -88,23 +99,25 @@
   (exwm-config-default)
 
   (setq exwm-input-global-keys
-	`(
-	  ;; Toggle between char and line mode:
-	  ([?\s-i] . exwm-input-toggle-keyboard)
-	  ;; Launch appliction:
-	  ([?\s-p] . (lambda (command)
-		       (interactive (list (read-shell-command "$ ")))
-		       (start-process-shell-command command nil command)))
-	  ;; Switch to certain workspace N:
-	  ,@(mapcar (lambda (i)
-		      `(,(kbd (format "s-%d" i)) .
-			(lambda ()
-			  (interactive)
-			  (exwm-workspace-switch-create ,i))))
-		    (number-sequence 0 9))))
+        `(
+          ;; Toggle between char and line mode:
+          ([?\s-i] . exwm-input-toggle-keyboard)
+          ;; Launch appliction:
+          ([?\s-p] . (lambda (command)
+                       (interactive (list (read-shell-command "$ ")))
+                       (start-process-shell-command command nil command)))
+          ;; Launch appliction:
+          ;; FIMXE: ([?\s-s-return] . (start-process-shell-command "xterm" nil "xterm"))
+          ;; Switch to certain workspace N:
+          ,@(mapcar (lambda (i)
+                      `(,(kbd (format "s-%d" i)) .
+                        (lambda ()
+                          (interactive)
+                          (exwm-workspace-switch-create ,i))))
+                    (number-sequence 0 9))))
 
   (setq window-divider-default-bottom-width 2
-	window-divider-default-right-width 2)
+        window-divider-default-right-width 2)
   (window-divider-mode))
 
 (use-package desktop-environment
@@ -114,23 +127,23 @@
   (desktop-environment-mode)
   :config
   (setq desktop-environment-volume-get-command "pulsemixer --get-volume"
-	desktop-environment-volume-get-regexp "\\([0-9]+\\)"
-	desktop-environment-volume-set-command "pulsemixer --change-colume %s"
-	desktop-environment-volume-toggle-command "pulsemixer --toggle-mute"
-	desktop-environment-volume-normal-increment "+5"
-	desktop-environment-volume-normal-decrement "-5"
-	desktop-environment-volume-small-increment "+1"
-	desktop-environment-volume-small-decrement "-1"
-	desktop-environment-screenshot-directory "~/pic"
-	desktop-environment-screenshot-command "maim ~/pic/sc_$(date +'%Y-%m-%d-%H%M%S.png')"
-	desktop-environment-screenshot-partial-command "maim -s  ~/pic/sc_$(date +'%Y-%m-%d-%H%M%S.png')"))
+        desktop-environment-volume-get-regexp "\\([0-9]+\\)"
+        desktop-environment-volume-set-command "pulsemixer --change-colume %s"
+        desktop-environment-volume-toggle-command "pulsemixer --toggle-mute"
+        desktop-environment-volume-normal-increment "+5"
+        desktop-environment-volume-normal-decrement "-5"
+        desktop-environment-volume-small-increment "+1"
+        desktop-environment-volume-small-decrement "-1"
+        desktop-environment-screenshot-directory "~/pic"
+        desktop-environment-screenshot-command "maim ~/pic/sc_$(date +'%Y-%m-%d-%H%M%S.png')"
+        desktop-environment-screenshot-partial-command "maim -s  ~/pic/sc_$(date +'%Y-%m-%d-%H%M%S.png')"))
 
 (use-package moody
   :ensure t
   :pin melpa-stable
   :config
   (setq x-underline-at-descent-line t
-	moody-mode-line-height 20)
+        moody-mode-line-height 20)
   (moody-replace-mode-line-buffer-identification)
   (moody-replace-vc-mode)
   (set-face-attribute 'mode-line nil :box nil)
