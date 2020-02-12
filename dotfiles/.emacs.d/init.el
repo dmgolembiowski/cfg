407a4a8c78c8a0abee49f5d116e9c796630d4dfd
Eivind Uggedal <eivind@uggedal.com>
2020-02-12 14:53:33 +0100

    emacs: improve startup performance

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -1,4 +1,13 @@
 ;;
+;; Init settings
+;;
+
+(setq gc-cons-threshold-original gc-cons-threshold)
+(setq gc-cons-threshold (* 1024 1024 100))
+(setq file-name-handler-alist-original file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+;;
 ;; Packages
 ;;
 
@@ -356,3 +365,16 @@
   :init
   (when (file-exists-p custom-file)
     (load custom-file)))
+
+;;
+;; Reset init settings
+;;
+
+(run-with-idle-timer
+ 5 nil
+ (lambda ()
+   (setq gc-cons-threshold gc-cons-threshold-original)
+   (setq file-name-handler-alist file-name-handler-alist-original)
+   (makunbound 'gc-cons-threshold-original)
+   (makunbound 'file-name-handler-alist-original)
+   (message "gc-cons-threshold and file-name-handler-alist restored")))
