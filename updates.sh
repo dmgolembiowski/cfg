00adae8b849db5a0edfe8fc44a16eb6d40ede113
Eivind Uggedal <eivind@uggedal.com>
2020-04-09 08:18:52 +0200

    updates.sh: pypi support

--- updates.sh
+++ updates.sh
@@ -4,6 +4,19 @@ ROOT=$(cd "$(dirname "$0")"; pwd -P)
 
 . $ROOT/ver.sh
 
+_cmp() {
+	local n="$1"
+	local _latest="$2"
+	local _cur="$3"
+
+	if [ -z "$_cur" ]; then
+		eval _cur=\$$(echo $n | tr '[a-z]' '[A-Z]' | sed 's/[^A-Z]//g')_V
+	fi
+
+	if [ "$_cur" != "$_latest" ]; then
+		echo $n $_cur '->' $_latest
+	fi
+}
 
 gh() {
 	local u="$1"
@@ -14,15 +27,21 @@ gh() {
 		curl -s https://api.github.com/repos/$u/$n/releases/latest |
 			jq .tag_name -j |
 			sed 's/^v//'
-		  )
+	)
 
-	if [ -z "$_cur" ]; then
-		eval _cur=\$$(echo $n | tr '[a-z]' '[A-Z]')_V
-	fi
+	_cmp $n "$_latest" "$_cur"
+}
 
-	if [ "$_cur" != "$_latest" ]; then
-		echo $n $_cur '->' $_latest
-	fi
+pypi() {
+	local n="$1"
+	local _cur="$2"
+
+	local _latest=$(
+		curl -s https://pypi.org/pypi/$n/json |
+			jq .info.version -r
+	)
+
+	_cmp $n "$_latest" "$_cur"
 }
 
 for n in prometheus alertmanager node_exporter; do
