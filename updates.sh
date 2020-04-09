e16f04cbd5eac7a3cc841987d9dda38e1ea1efdc
Eivind Uggedal <eivind@uggedal.com>
2020-04-09 08:24:59 +0200

    updates.sh: show service type

--- updates.sh
+++ updates.sh
@@ -5,22 +5,23 @@ ROOT=$(cd "$(dirname "$0")"; pwd -P)
 . $ROOT/ver.sh
 
 _cmp() {
-	local n="$1"
-	local _latest="$2"
-	local _cur="$3"
+	local t=$1
+	local n=$2
+	local _latest="$3"
+	local _cur="$4"
 
 	if [ -z "$_cur" ]; then
 		eval _cur=\$$(echo $n | tr '[a-z]' '[A-Z]' | sed 's/[^A-Z]//g')_V
 	fi
 
 	if [ "$_cur" != "$_latest" ]; then
-		echo $n $_cur '->' $_latest
+		echo $t $n $_cur '->' $_latest
 	fi
 }
 
 gh() {
-	local u="$1"
-	local n="$2"
+	local u=$1
+	local n=$2
 	local _cur="$3"
 
 	local _latest=$(
@@ -29,11 +30,11 @@ gh() {
 			sed 's/^v//'
 	)
 
-	_cmp $n "$_latest" "$_cur"
+	_cmp gh $n "$_latest" "$_cur"
 }
 
 pypi() {
-	local n="$1"
+	local n=$1
 	local _cur="$2"
 
 	local _latest=$(
@@ -41,7 +42,7 @@ pypi() {
 			jq .info.version -r
 	)
 
-	_cmp $n "$_latest" "$_cur"
+	_cmp py $n "$_latest" "$_cur"
 }
 
 for n in prometheus alertmanager node_exporter; do
